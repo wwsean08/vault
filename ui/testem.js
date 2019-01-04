6e04cfb1f16b486a7dd26adbdadff8b3f08457fc
Noelle Daley <adriannenoelle@gmail.com>
2019-01-04 10:36:31 -0800

    install browserstack

--- ui/testem.js
+++ ui/testem.js
@@ -3,24 +3,16 @@ const config = {
   test_page: 'tests/index.html?hidepassed',
   tap_quiet_logs: true,
   disable_watching: true,
-  launch_in_ci: ['Chrome'],
-  browser_args: {
-    Chrome: {
-      ci: [
-        // --no-sandbox is needed when running Chrome inside a container
-        process.env.CI ? '--no-sandbox' : null,
-        '--headless',
-        '--disable-gpu',
-        '--disable-software-rasterizer',
-        '--mute-audio',
-        '--remote-debugging-port=0',
-        '--window-size=1440,900',
-      ].filter(Boolean),
+  launchers: {
+    bs_chrome: {
+      exe: 'node_modules/.bin/browserstack-launch',
+      args: ['--os', 'Windows', '--osv', '10', '--b', 'chrome', '--bv', 'latest', '-t', '600', '--u'],
+      protocol: 'browser',
     },
   },
+  launch_in_ci: ['Chrome'],
   on_exit:
     '[ -e ../../vault-ui-integration-server.pid ] && node ../../scripts/start-vault.js `cat ../../vault-ui-integration-server.pid`; [ -e ../../vault-ui-integration-server.pid ] && rm ../../vault-ui-integration-server.pid',
-
   proxies: {
     '/v1': {
       target: 'http://localhost:9200',
