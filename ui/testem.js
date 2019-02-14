9b4e3e645d9e6043f4f77296b0c0ad854bc94dc0
Noelle Daley <adriannenoelle@gmail.com>
2019-02-14 13:54:59 -0800

    separate testems for browserstack and local testing

--- ui/testem.js
+++ ui/testem.js
@@ -3,98 +3,28 @@ const config = {
   test_page: 'tests/index.html?hidepassed',
   tap_quiet_logs: true,
   disable_watching: true,
-  timeout: 1200,
-  browser_start_timeout: 2000,
-  parallel: 2,
-  launchers: {
-    BS_Chrome_Current: {
-      exe: 'node_modules/.bin/browserstack-launch',
-      args: [
-        '--os',
-        'Windows',
-        '--osv',
-        '10',
-        '--b',
-        'chrome',
-        '--bv',
-        'latest',
-        '-t',
-        '600',
-        '--u',
-        '<url>',
-      ],
-      protocol: 'browser',
-    },
-    BS_Firefox_Current: {
-      exe: 'node_modules/.bin/browserstack-launch',
-      args: [
-        '--os',
-        'Windows',
-        '--osv',
-        '10',
-        '--b',
-        'firefox',
-        '--bv',
-        'latest',
-        '-t',
-        '600',
-        '--u',
-        '<url>',
-      ],
-      protocol: 'browser',
-    },
-    BS_Safari_Current: {
-      exe: 'node_modules/.bin/browserstack-launch',
-      args: [
-        '--os',
-        'OS X',
-        '--osv',
-        'Mojave',
-        '--b',
-        'safari',
-        '--bv',
-        'latest',
-        '-t',
-        '600',
-        '--u',
-        '<url>',
-      ],
-      protocol: 'browser',
-    },
-    BS_IE_11: {
-      exe: 'node_modules/.bin/browserstack-launch',
-      args: [
-        '--os',
-        'Windows',
-        '--osv',
-        '10',
-        '--b',
-        'ie',
-        '--bv',
-        '11.0',
-        '-t',
-        '1500',
-        '--u',
-        '<url>&legacy=true',
-      ],
-      protocol: 'browser',
+  launch_in_ci: ['Chrome'],
+  browser_args: {
+    Chrome: {
+      ci: [
+        // --no-sandbox is needed when running Chrome inside a container
+        process.env.CI ? '--no-sandbox' : null,
+        '--headless',
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--disable-software-rasterizer',
+        '--mute-audio',
+        '--remote-debugging-port=0',
+        '--window-size=1440,900',
+      ].filter(Boolean),
     },
   },
-  launch_in_dev: [],
-  launch_in_ci: ['BS_Chrome_Current', 'BS_Firefox_Current', 'BS_Safari_Current', 'BS_IE_11'],
   on_exit:
     '[ -e ../../vault-ui-integration-server.pid ] && node ../../scripts/start-vault.js `cat ../../vault-ui-integration-server.pid`; [ -e ../../vault-ui-integration-server.pid ] && rm ../../vault-ui-integration-server.pid',
+
   proxies: {
     '/v1': {
       target: 'http://localhost:9200',
     },
   },
 };
-
-if (process.env.CI) {
-  config.reporter = 'xunit';
-  config.report_file = 'test-reports/ember.xml';
-  config.xunit_intermediate_output = true;
-}
-
-module.exports = config;
